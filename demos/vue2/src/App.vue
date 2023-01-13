5422750f7d37d38a06296222c56ae62c07b4944e
Devin Deng <awesomedevin.deng@gmail.com>
2023-01-13 18:27:19 +0800

    feat: 新增vue2 Demo

--- demos/vue2/src/App.vue
+++ demos/vue2/src/App.vue
@@ -1,18 +1,16 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import Vue from 'vue'
+
+console.log('Vue',Vue)
+
 </script>
 
 <template>
   <div id="app">
     <header>
       <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-      <div class="wrapper">
-        <HelloWorld msg="You did it!" />
-      </div>
     </header>
-
     <main>
       <TheWelcome />
     </main>
---
+++ demos/vue2/src/components/NameForm.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="hello">
+    <h3>NameForm Component</h3>
+    <div>
+      <input
+        @input="handleChange1"
+        placeholder="请输入您的名称"
+        :value="name"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import Store from "../useStore";
+
+const setName = Store.useStore((state) => state.setName);
+
+export default {
+  name: "Form",
+  data() {
+    return {
+      name: Store.useStore((state) => state.userInfo.name),
+      store: Store.useStore(),
+      value: "",
+      setTitle: Store.useStore((state) => state.setTitle),
+    };
+  },
+
+  methods: {
+    handleChange1(e) {
+      // this.name = e.target.value; // not working
+      this.store.setName(e.target.value);
+    },
+    handleChange2(e) {
+      this.value = e.target.value;
+    },
+  },
+
+  mounted() {
+    this.setTitle("Demo");
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+button {
+  margin-left: 20px;
+}
+</style>
