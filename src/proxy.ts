59fd6aa629ddc2c84799c63ae8c874cf78b2911b
AwesomeDevin <awesomedevin.deng@gmail.com>
2023-01-27 13:29:17 +0800

    feat: update docs

--- src/proxy.ts
+++ src/proxy.ts
@@ -5,7 +5,6 @@ import { ExtractState, TSubscribeCache, TObject } from "./vue";
 
 
 export function defineProxy<T, S extends TObject>(store: S, subscribeCache: TSubscribeCache, api: StoreApi<T>, selection: (state: T) => S) {
-  console.log('proxy1234')
   const keys = Object.keys(store)
   const reactiveStore = Vue.reactive(store);
   const val = new Proxy(reactiveStore, {
