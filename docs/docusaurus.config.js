d1d61941659eaade457f9d3bca1137e638a02d2e
AwesomeDevin <awesomedevin.deng@gmail.com>
2023-02-16 17:28:29 +0800

    docs: update

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: '🐻 ZUSTAND',
+  title: 'ZUSTAND',
   url: 'https://awesomedevin.github.io',
   baseUrl: '/zustand-vue/',
   onBrokenLinks: 'throw',
